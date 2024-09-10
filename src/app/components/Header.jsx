a9ee0069fddfd689989472799c5ee3ae0489d0e2
guspirasak <pirasak.s@9expert.co.th>
2024-09-10 16:55:59 +0700

    update 0.2

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -3,8 +3,10 @@ import React, { Component } from 'react'
 export class Header extends Component {
   render() {
     return (
-      <div>
-        
+        <div className='container mx-auto'>
+        <h3>Contact Page</h3>
+        <p>This is Contact page</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi ex accusantium eius ad qui esse deleniti eligendi fugit aliquid dolorum incidunt consectetur molestiae, excepturi tempora aspernatur tenetur quod debitis similique?</p>
       </div>
     )
   }
