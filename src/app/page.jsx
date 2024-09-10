e3cac91b45307b528b7d4934832578aa84b36f23
guspirasak <pirasak.s@9expert.co.th>
2024-09-10 15:02:26 +0700

    อัพเดท

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,30 @@
 import Image from "next/image";
 import Link from 'next/link'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   return (
       <main className="flex">
         <Link href="/about">Go to about Page</Link>
+        <br />
         <Link href="/services">Go to services Page</Link>
+        <br />
         <Link href="/blog">Go to blog Page</Link>
+        <br />
+        <Link href="/contact">Go to contact Page</Link>
+        <Link href="/about">Go to about Page</Link>
+        <br />
+        <Link href="/services">Go to services Page</Link>
+        <br />
+        <Link href="/blog">Go to blog Page</Link>
+        <br />
+        <Link href="/contact">Go to contact Page</Link>
+        <Link href="/about">Go to about Page</Link>
+        <br />
+        <Link href="/services">Go to services Page</Link>
+        <br />
+        <Link href="/blog">Go to blog Page</Link>
+        <br />
         <Link href="/contact">Go to contact Page</Link>
     </main>
   );
